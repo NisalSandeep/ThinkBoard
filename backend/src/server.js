9b6303944c034f30cba51002734a5cdb03e29c48
NisalSandeep <sandeep-as22018@stu.kln.ac.lk>
2026-03-06 17:20:55 +0530

    prepared deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,24 +1,27 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const path = require("path");
 
 const noteRoutes = require("./routes/noteRoutes");
 const connectDB = require("./config/db");
 const rateLimiter = require("./middleware/rateLimiter");
+const __dirname = path.resolve();
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  }),
-);
+if(process.env.NODE_ENV !== "production") {
+    app.use(cors({
+        origin: "http://localhost:5173",
+    }));
+} 
+
+
 app.use(express.json());
 app.use(rateLimiter);
-
 
 app.use((req, res, next) => {
   console.log("We have a new request!");
@@ -26,6 +29,16 @@ app.use((req, res, next) => {
 });
 
 app.use("/api/notes", noteRoutes);
+
+
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 connectDB()
   .then(() => {
